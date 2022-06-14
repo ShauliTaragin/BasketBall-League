3cef11dc170fc570884ef237359ca6009d9d9f53
ShauliTaragin <shaulitaragin@gmail.com>
2022-06-14 11:33:24 +0300

    moving repo

--- sources/Game.hpp
+++ sources/Game.hpp
@@ -12,11 +12,11 @@ namespace taragin{
         Game(Team* home , Team* away): home_team(home) , away_team(away){}
 
         &Team Play(){
-            int score_home = Score(55 , 100);
-            int score_away = Score(50 , 100);
+            int score_home = Score(55 , 100 , *home_team);
+            int score_away = Score(50 , 100 , *away_team);
 
         }
-        int Score(int first , int second){
+        int Score(int first , int second , Team team){
                 int r = (rand()%(B-A)) + A;
                 return r;
         }
